08be6b9c391482e85e0e1f543473845340ed59b0
BrandonMGH <BrandonMGH@Yahoo.com>
2020-03-22 18:40:05 -0700

    update

--- typescript/main.js
+++ typescript/main.js
@@ -1,4 +1,14 @@
-var test = function () {
-    console.log("Hello");
-};
-test();
+var Cat = /** @class */ (function () {
+    function Cat(id, name, weight, height) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+    Cat.prototype.getInfo = function () {
+        console.log(this.id, this.name, this.weight, this.height);
+    };
+    return Cat;
+}());
+var Sinatra = new Cat(1, "Sinatra", 2, 10);
+Sinatra.getInfo();
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,5 +1,25 @@
-let test = () => {
-    console.log("Hello")
+
+class Cat {
+    id: number;
+    name: string; 
+    weight: number;
+    height: number;
+    
+    constructor(  id: number, 
+        name: string,
+        weight: number,
+        height: number){
+            this.id = id; 
+            this.name = name;
+            this.weight = weight;
+            this.height = height; 
+
+    }
+    getInfo(): void{
+        console.log(this.id, this.name, this.weight, this.height)
+    }
 }
 
-test();
+const Sinatra = new Cat (1,"Sinatra",2, 10)
+
+Sinatra.getInfo()
